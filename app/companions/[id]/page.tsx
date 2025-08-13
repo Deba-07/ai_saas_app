1863da1c4b78dc0f9e4b149a5e5c959526fd834b
Deba-07 <dynamicdeba07@gmail.com>
2025-08-13 08:41:56 +0530

    implement companion component

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,3 +1,4 @@
+import CompanionComponent from "@/components/CompanionComponent";
 import { getCompanion } from "@/lib/actions/companions.actions";
 import { getSubjectColor } from "@/lib/utils";
 import { currentUser } from "@clerk/nextjs/server";
@@ -13,7 +14,8 @@ const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
   const companion = await getCompanion(id);
   const user = await currentUser();
 
-  console.log(companion)
+  const { name, subject, title, topic, duration } = companion;
+  console.log(companion);
 
   if (!user) redirect("/sign-in");
 
@@ -24,24 +26,33 @@ const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
         <div className="flex items-center gap-2">
           <div
             className="size-[72px] flex items-center justify-center rounded-lg max-md:hidden"
-            style={{ backgroundColor: getSubjectColor(companion.subject) }}
+            style={{ backgroundColor: getSubjectColor(subject) }}
           >
-            <Image src={`/icons/${companion.subject}.svg`} alt={companion.subject} width={35} height={35} />
+            <Image
+              src={`/icons/${subject}.svg`}
+              alt={subject}
+              width={35}
+              height={35}
+            />
           </div>
           <div className="flex flex-col gap-2">
             <div className="flex items-center gap-2">
-              <p className="font-bold text-2xl">{companion.name}</p>
-              <div className="subject-badge max-sm:hidden">
-                {companion.subject}
-              </div>
+              <p className="font-bold text-2xl">{name}</p>
+              <div className="subject-badge max-sm:hidden">{subject}</div>
             </div>
-            <p className="text-lg">{companion.topic}</p>
+            <p className="text-lg">{topic}</p>
           </div>
         </div>
         <div className="items-start text-2xl max-md:hidden">
-          {companion.duration} mins
+          {duration} mins
         </div>
       </article>
+      <CompanionComponent
+        {...companion}
+        companionId={id}
+        userName={user.firstName}
+        userImage={user.imageUrl}
+      />
     </main>
   );
 };
